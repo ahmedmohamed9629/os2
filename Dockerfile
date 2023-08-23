f606704b303c4ac69824922b243c1bbe16cd6091
A7MED MOHAMED <awd896541@gmail.com>
2023-08-23 03:23:46 +0300

    checkout

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM alpine
+FROM openjdk
+
+WORKDIR /application
+
+COPY task.java .
+
+RUN javac task.java 
+
+CMD java task
---
+++ task.java
@@ -0,0 +1,5 @@
+public class task{
+    public static void main(String[] args){
+        System.out.println("A7MED MOHAMED ELSAYED HASSAN");
+    }
+}
